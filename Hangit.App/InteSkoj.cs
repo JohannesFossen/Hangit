fdd245b64667dfde7739a02ddc98e8842efe584c
NORCONSULTAD\jofos <johannes.fossen@norconsult.com>
2020-10-07 15:22:36 +0200

    Pluss InteSkoj.

---
+++ Hangit.App/InteSkoj.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hangit.App
+{
+    class InteSkoj
+    {
+        public void WriteSkoj()
+        {
+            Console.WriteLine("Inte skoj...");
+        }
+    }
+}
