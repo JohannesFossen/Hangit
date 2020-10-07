1ce2fee5fcbda85b749b5c3e0c3ff95ad4373258
NORCONSULTAD\jofos <johannes.fossen@norconsult.com>
2020-10-07 10:15:48 +0200

    Method ShowSolution.

--- Hangit.App/UserIO.cs
+++ Hangit.App/UserIO.cs
@@ -6,5 +6,11 @@ namespace Hangit.App
 {
     class UserIO
     {
+        public ShowSolution(char[] inSolution)
+        {
+            foreach (char c in inSolution)
+                Console.Write($"{c} ");
+            Console.WriteLine("");
+        }
     }
 }
