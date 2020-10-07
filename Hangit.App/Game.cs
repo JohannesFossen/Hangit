382d0aabcdff069b4984155f04cf1af3c25c2acc
NORCONSULTAD\jofos <johannes.fossen@norconsult.com>
2020-10-07 14:49:00 +0200

    Games with added comments.

--- Hangit.App/Game.cs
+++ Hangit.App/Game.cs
@@ -7,8 +7,8 @@ namespace Hangit.App
     public class Game
     {
         private char[] _wordtoguess;
-        private char[] _solution = new char[30];
-        private char[] _guesses = "                              ".ToCharArray();
+        private char[] _scoreboard = new char[30];
+        private char[] _missed = "                              ".ToCharArray();
         private int _triesleft = Program.maxTries;
 
         public Game(string inWord)
@@ -16,8 +16,8 @@ namespace Hangit.App
             _wordtoguess = inWord.ToUpper().ToCharArray();
             int i = 0;
             foreach (char c in _wordtoguess)
-                _solution[i++] = '-';
-            Console.WriteLine(_wordtoguess);
+                _scoreboard[i++] = '-';
+            Console.WriteLine(_wordtoguess); // Just for test...
         }
         public void Play()
         {
@@ -26,18 +26,21 @@ namespace Hangit.App
             UserIO.ShowText("Starting the guess!");
             do
             {
-                UserIO.ShowCharString(_solution);
-                UserIO.ShowCharString(_guesses);
+                // Show current score.
+                UserIO.ShowCharString(_scoreboard);
+                UserIO.ShowCharString(_missed);
+                // Read one key and check if tried already.
                 do
                 {
                     key = UserIO.ReadChar(_triesleft);
-                    if (Evaluate.GetCharPos(_guesses, key) >= 0)
+                    if (Evaluate.GetCharPos(_missed, key) >= 0)
                     {
                         UserIO.ShowText(" Tried already.");
                         key = ' ';
                     }
                 } while (key == ' ');
-                if (Evaluate.CheckSolution(key, _wordtoguess, _solution))
+                // Check if key is a hit.
+                if (Evaluate.CheckSolution(key, _wordtoguess, _scoreboard))
                 {
                     UserIO.ShowText(" Hit!");
                     _triesleft--;
@@ -45,12 +48,18 @@ namespace Hangit.App
                 else
                 {
                     UserIO.ShowText(" Miss.");
-                    Evaluate.AddCharValue(_guesses, key);
+                    Evaluate.AddCharValue(_missed, key);
                 }
-                if (Evaluate.GetCharPos(_solution,'-') == -1)
+                // Winner or bust?
+                if (Evaluate.GetCharPos(_scoreboard,'-') == -1)
                 {
-                    UserIO.ShowCharString(_solution);
+                    UserIO.ShowCharString(_scoreboard);
                     UserIO.ShowText("Winner!");
+                    done = true;
+                }else if (_triesleft == 0)
+                {
+                    UserIO.ShowText($"Sorry, you spent all {Program.maxTries} tries. The solution was:");
+                    UserIO.ShowCharString(_wordtoguess);
                     done = true;
                 }
             } while (!done);
