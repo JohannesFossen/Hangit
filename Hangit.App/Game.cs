97d4e46bc1cc330808ab2ceaece6b1eaaf20bec2
NORCONSULTAD\jofos <johannes.fossen@norconsult.com>
2020-10-07 11:04:36 +0200

    Revert "Game gets GetCharPos."
    This reverts commit 23f12d48a9f334b000cb954a87d188c977a9a3bc.

--- Hangit.App/Game.cs
+++ Hangit.App/Game.cs
@@ -11,7 +11,6 @@ namespace Hangit.App
         private char[] _guesses;
         private int _triesleft = Program.maxTries;
 
-        #region constructor
         public Game(string inWord)
         {
             _wordtoguess = inWord.ToUpper().ToCharArray();
@@ -19,15 +18,6 @@ namespace Hangit.App
             foreach (char c in _wordtoguess)
                 _solution[i++] = '-';
             Console.WriteLine(_wordtoguess);
-        }
-        #endregion
-
-        internal int GetCharPos(char[] inCharList,char find,int start=0)
-        {
-            for (int i=start;i<inCharList.Length; i++)
-                if (inCharList[i] == find)
-                    return i;
-            return -1;
         }
     }
 }
