568a07c81430e708aaef7968a80d21c2a2f390a2
NORCONSULTAD\jofos <johannes.fossen@norconsult.com>
2020-10-07 14:01:13 +0200

    New method ReadChar().

--- Hangit.App/UserIO.cs
+++ Hangit.App/UserIO.cs
@@ -12,5 +12,27 @@ namespace Hangit.App
                 Console.Write($"{c} ");
             Console.WriteLine("");
         }
+        public static char ReadChar()
+        {
+            char key;
+            //string str;
+            do
+            {
+                Console.Write("Next one to try > ");
+                key = Console.ReadKey().KeyChar;
+                //Console.WriteLine("");
+                key = (key.ToString().ToUpper())[0];
+                if (key < 'A' || key > 'Z')
+                {
+                    Console.WriteLine($" Illegal input '{key}'.");
+                    key = ' ';
+                }
+            } while (key == ' ');
+            return key;
+        }
+        public static void ShowText(string inText)
+        {
+            Console.WriteLine(inText);
+        }
     }
 }
